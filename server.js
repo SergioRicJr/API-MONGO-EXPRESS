1408cad71a8668f6a8597c1f140a00931344bbe5
Sergio <sergioricjr7@gmil.com>
2023-02-22 16:32:59 -0300

    continuando api criando rotas e verbos http

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const http = require("http") //importação
-const port = 3000  //porta para escutar as requisições, escolher a porta -- da pra criar variiavel q define porta automaticamente
-// instalar nodemon p salvar e atualizar servidor --- -D p nao ir pra producao
-const rotas = {
-    '/': 'Curso de node',
-    '/livros': 'entrei na página de livros', //dando erro na acentuacao 
-    '/autores': 'listagem de autores',
-    '/editora': 'página de editora'
-}
+// const http = require("http") //importação
+// const port = 3000  //porta para escutar as requisições, escolher a porta -- da pra criar variiavel q define porta automaticamente
+// // instalar nodemon p salvar e atualizar servidor --- -D p nao ir pra producao
+// const rotas = {
+//     '/': 'Curso de node',
+//     '/livros': 'entrei na página de livros', //dando erro na acentuacao 
+//     '/autores': 'listagem de autores',
+//     '/editora': 'página de editora'
+// }
 
-//sempre q o server receber requisicao essa funcao callback será passada
-const server = http.createServer((req,res)=>{ //função de callback, oq devolver quando chamar o server
-    res.writeHead(200, {"Content-Type": "text=plain"})//define status, 200 ok, e tipo de conteudo 
-    res.end(rotas[req.url]) //end para definir oq mandar para o navegador
-    //definiu as rotas, se tal rota, tal mensagem e passa url como parametro
-})
+// //sempre q o server receber requisicao essa funcao callback será passada
+// const server = http.createServer((req,res)=>{ //função de callback, oq devolver quando chamar o server
+//     res.writeHead(200, {"Content-Type": "text=plain"})//define status, 200 ok, e tipo de conteudo 
+//     res.end(rotas[req.url]) //end para definir oq mandar para o navegador
+//     //definiu as rotas, se tal rota, tal mensagem e passa url como parametro
+// })
 
-server.listen(port, ()=>{ //p rodar servidor na porta e definir callback quando der certo
-console.log(`servidor escutando em http://localhost:${port}`)
+// server.listen(port, ()=>{ //p rodar servidor na porta e definir callback quando der certo
+// console.log(`servidor escutando em http://localhost:${port}`)
+// })
+
+
+import app from "./src/app.js"
+
+const port = process.env.PORT || 3000 //para escolher automaticamente a porta
+
+app.listen(port, ()=>{
+    console.log(`Servidor escutando em http://localhost:${port}`)
 })
---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json()) //para que seja interpretado oq chega via post ou put em json p consegui manipular e armazenar
+//app.use para conseguir adicionar
+const livros = [
+    {id: 1, "titulo": "Senhor dos Aneis"},
+    {id:2, "titulo": "O hobbit"}
+] 
+
+app.get("/", (req, res)=>{
+    res.status(200).send("Curso de Node");
+})
+
+app.get("/livros", (req, res)=>{
+    res.status(200).json(livros) //a resposta vai ser um json
+})
+
+app.get("/livros/:id", (req, res)=>{ // :id para indicar que esse parametro vai chegar depois
+    let index = buscaLivro(req.params.id)
+    res.json(livros[index])// o default é status 200
+})
+
+app.post("/livros", (req, res)=>{
+    livros.push(req.body) //vai adicionar o corpo da requisicao nos livros
+    res.status(201).send("o livro foi cadastrado com sucesso") //201 ok, criado
+})
+// no put vem parametros no URI e no corpo da requisição
+app.put("/livros/:id", (req, res)=>{ // :id para indicar que esse parametro vai chegar depois
+    let index = buscaLivro(req.params.id)
+    livros[index].titulo = req.body.titulo 
+    res.status(200).json(livros)// o default é status 200
+})
+
+function buscaLivro(id) {
+    return livros.findIndex((livro) => livro.id == id) //pede uma funcao para comparar oq é aquele elemento
+} //para cada item na funcao vai chamar de livro e ver se bate com o id inserido
+
+export default app //vai exportar tudo que tem aqui para outros
